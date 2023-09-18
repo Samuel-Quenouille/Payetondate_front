3e54bb3d13111dc148ea78e9ff85cdf45265a65d
Amandine Fernandes <fernandesamandine13@gmail.com>
2023-09-18 18:41:47 +0200

    logo

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import '../index.css';
 
 export default function Welcome() {
 
-  return (
+    return (
     <div className='container welcome'>
       <img className='logo' src='src/assets/img/logo.png' alt='Logo' style={{ display: 'block', margin: '0 auto', width: '30%' }}/>
       <h2 className='welcome-title'>Repère le lieu idéal pour<br></br>LE Date !</h2>
@@ -12,9 +12,6 @@ export default function Welcome() {
       <p className='welcome-text'>
         5 catégories, 5 types de date. A vous de choisir !
       </p>
-      <div>
-
-      </div>
     </div>
   );
 }
