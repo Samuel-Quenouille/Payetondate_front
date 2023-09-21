c0037f2054e515c4a7b485fa8a5cf0e82e6214d5
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-21 13:08:52 +0200

    remove kiss

--- src/pages/kiss.jsx
+++ src/pages/kiss.jsx
@@ -39,6 +39,27 @@ export default function Kiss() {
         <div className="container">
             <img src={cloudIcon} alt="Cloud Icon" className="category-icon" style={{ width: '5%', position: 'absolute', marginLeft: '510px', marginTop: '-15px' }} />
             <h1 className="kiss-title" style={{position: 'relative'}}>Spots à bisou(s)</h1>
+
+            <div className="image-and-content">
+                <img src={beaubourg} alt='Musée Beaubourg' style={{width:'20%'}} className='buttes-chaumont-img' />
+                <div className="content">
+                    <p alt='Photo cabine' style={{color:'white'}}><b>Le centre Pompidou</b>, Place Georges-Pompidou, 75004 Paris</p>
+                    <p className='spot-description'>Découvrez ensemble l'art contemporain sous toutes ses facettes et laissez-vous emporter par la créativité et l'inspiration qui émanent de ce lieu emblématique de Paris.</p>
+                    <p>Fourchette de prix: 12 - 18€</p>
+                    <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
+                </div>
+            </div>
+
+            <div className="image-and-content">
+                <img src={buttes_chaumont} alt='buttes chaumont' style={{width:'20%'}} className='buttes-chaumont-img' />
+                <div className="content">
+                    <p alt='Photo cabine' style={{color:'white'}}><b>Photo cabine</b>, Avenue Darcel, parc des Buttes Chaumonts, 75019 Paris</p>
+                    <p className='spot-description'>Situé dans le parc des Buttes Chaumont au pied du Pavillon Puebla, ce photomaton est idéal pour capturer un moment magique.</p>
+                    <p>Fourchette de prix: 3€</p>
+                    <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
+                </div>
+            </div>
+
             {places.map((place) => (  
             <div className="image-and-content" key={place.id}>
                 <img src={beaubourg} alt='Musée Beaubourg' style={{width:'20%'}} className='buttes-chaumont-img' />
@@ -47,26 +68,16 @@ export default function Kiss() {
                     <br></br>
                     <p alt='Photo cabine' style={{color:'white'}}>{place.address}, {place.city} {place.borough}</p>
                     <p className='spot-description'>{place.description}</p>
-                    <p>Fourchette de prix : {place.price}</p>
+                    <p>Fourchette de prix: {place.price}</p>
                     <br></br>
-                <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
-            </div>
+                    <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
+                </div>
             </div>
             ))}
             <div className="return d-flex justify-content-end">
                 <Link to="/">
                     <span>Retour</span>
                 </Link>
-            </div>
-            
-            <div className="image-and-content">
-                <img src={buttes_chaumont} alt='buttes chaumont' style={{width:'20%'}} className='buttes-chaumont-img' />
-                <div className="content">
-                    <p alt='Photo cabine' style={{color:'white'}}>Photo cabine, Avenue Darcel, parc des Buttes Chaumonts, 75019 Paris</p>
-                    <p className='spot-description'>Situé dans le parc des Buttes Chaumont au pied du Pavillon Puebla, ce photomaton est idéal pour capturer un moment magique.</p>
-                    <p>Fouchette de prix: 3€</p>
-                    <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
-                </div>
             </div>
         </div>
     );
