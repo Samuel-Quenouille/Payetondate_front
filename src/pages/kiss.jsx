b1386c5d2f6f593a26bf173d68c8210fadbb0a2e
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-21 12:24:59 +0200

    change kiss page and pictures categories

--- src/pages/kiss.jsx
+++ src/pages/kiss.jsx
@@ -37,23 +37,28 @@ export default function Kiss() {
 
     return (
         <div className="container">
+            <img src={cloudIcon} alt="Cloud Icon" className="category-icon" style={{ width: '5%', position: 'absolute', marginLeft: '510px', marginTop: '-15px' }} />
+            <h1 className="kiss-title" style={{position: 'relative'}}>Spots à bisou(s)</h1>
+            {places.map((place) => (  
+            <div className="image-and-content" key={place.id}>
+                <img src={beaubourg} alt='Musée Beaubourg' style={{width:'20%'}} className='buttes-chaumont-img' />
+                <div className="content">
+                    <h3>{place.title}</h3>
+                    <br></br>
+                    <p alt='Photo cabine' style={{color:'white'}}>{place.address}, {place.city} {place.borough}</p>
+                    <p className='spot-description'>{place.description}</p>
+                    <p>Fourchette de prix : {place.price}</p>
+                    <br></br>
+                <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
+            </div>
+            </div>
+            ))}
             <div className="return d-flex justify-content-end">
                 <Link to="/">
                     <span>Retour</span>
                 </Link>
             </div>
-            <img src={cloudIcon} alt="Cloud Icon" className="category-icon" style={{ width: '5%', position: 'absolute', marginLeft: '510px', marginTop: '-15px' }} />
-                <h1 className="kiss-title" style={{position: 'relative'}}>Spots à bisou(s)</h1>
-                
-            <div className="image-and-content">
-                <img src={beaubourg} alt='Musée Beaubourg' style={{width:'20%'}} className='buttes-chaumont-img' />
-            <div className="content">
-                <p alt='Photo cabine' style={{color:'white'}}>Le centre Pompidou, Place Georges-Pompidou, 75004 Paris</p>
-                <p className='spot-description'>Découvrez ensemble l'art contemporain sous toutes ses facettes et laissez-vous emporter par la créativité et l'inspiration qui émanent de ce lieu emblématique de Paris. </p>
-                <p>Fourchette de prix: 12-18€</p>
-                <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
-            </div>
-            </div>
+            
             <div className="image-and-content">
                 <img src={buttes_chaumont} alt='buttes chaumont' style={{width:'20%'}} className='buttes-chaumont-img' />
                 <div className="content">
@@ -63,14 +68,6 @@ export default function Kiss() {
                     <button type="submit" className="circle-btn btn-map">Voir sur la map</button>
                 </div>
             </div>
-            {places.map((place) => (
-              <li key={place.id}>
-                <h3>{place.title}</h3>
-                <p className="text-kiss">Description : {place.description}</p>
-                <p className="text-kiss">Adresse : {place.address}, {place.city} {place.borough}</p>
-                <p className="text-kiss">Prix : {place.price}</p>
-              </li>
-            ))}
         </div>
     );
 }
